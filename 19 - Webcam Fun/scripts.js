ade2485546f530aab0bd23717f565dd7d4493f9a
GBS Admin <gbsadmin@Jays-MacBook-Pro.local>
2019-04-04 18:38:48 +0800

    enabled video streaming

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,25 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+if (navigator.getUserMedia) {
+  navigator.getUserMedia(
+    {
+      video: true
+    },
+    function (play) {
+      var video = document.querySelector('video');
+      video.srcObject = play;
+      video.onloadedmetadata = function (e) {
+        video.play();
+      };
+    },
+    function (err) {
+      console.log("Error: " + err.name);
+    }
+  );
+} else {
+  console.log("Not supported.");
+};
