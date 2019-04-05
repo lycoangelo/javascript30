6ca8d3749d0f724db1fc465ae7bc89f2a831439f
GBS Admin <gbsadmin@Jays-MacBook-Pro.local>
2019-04-05 12:11:17 +0800

    stream video and downloadable captured photos

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -2,15 +2,35 @@ const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+const snap = document.querySelector('.snap')
 
-constraints = { video: true };
-
-navigator.mediaDevices.getUserMedia(constraints)
+navigator.mediaDevices.getUserMedia({ video: true })
   .then(function (stream) {
     video.srcObject = stream;
     video.play();
+    video.onloadedmetadata = function (e) {
+      drawCanvas();
+    }
   })
   .catch(function (err) {
     console.log('Error:', err);
   });
+
+function drawCanvas() {
+  ctx.globalCompositionOperation = "lighten";
+  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+  let imageURL = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  const img = document.createElement('img');
+  img.setAttribute('src', imageURL);
+  const imgSRC = img.src;
+  link.setAttribute('href', imgSRC);
+  link.setAttribute('download', '');
+  document.body.append(link);
+  link.append(img);
+}
