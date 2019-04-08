36fe081b2e9c3007718c8e1403eebb60cfaa48b6
GBS Admin <gbsadmin@Jays-MacBook-Pro.local>
2019-04-08 13:13:50 +0800

    toggle

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -2,23 +2,59 @@ const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap')
+const snap = document.querySelector('.snap');
+const toggles = document.querySelectorAll('.effectToggle');
+let effects = [
+  { name: 'RE', state: false },
+  { name: 'RS', state: false },
+  { name: 'GS', state: false },
+  { name: 'DE', state: true }
+];
 
-navigator.mediaDevices.getUserMedia({ video: true })
-  .then(function (stream) {
-    video.srcObject = stream;
-    const videoPlay = video.play();
-    video.onloadedmetadata = function (e) {
-      drawCanvas(videoPlay);
+function toggleEffect() {
+  effects.map((effect, index) => {
+    if (effect.name === this.name) {
+      effect.state = true;
+    } else {
+      effect.state = false;
     }
   })
-  .catch(function (err) {
-    console.log('Error:', err);
-  });
+}
+
+toggles.forEach(toggle => toggle.addEventListener('click', toggleEffect));
+
+function displayVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+      const videoPlay = video.play();
+      video.onloadedmetadata = function (e) {
+        drawCanvas(videoPlay);
+      }
+    })
+    .catch(function (err) {
+      console.log('Error:', err);
+    });
+}
+
+displayVideo();
 
 function drawCanvas(videoPlay) {
   setInterval(() => {
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    let pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    effects.map(effect => {
+      if (effect.name == 'RE' && effect.state == true) {
+        pixels = redEffects(pixels);
+      } else if (effect.name == 'RS' && effect.state == true) {
+        pixels = rgbSplit(pixels);
+      } else if (effect.name == 'GS' && effect.state == true) {
+        pixels = greenScreen(pixels);
+      } else if (effect.name == 'DE' && effect.state == true) {
+        pixels = ctx.getImageData(0, 0, canvas.width, canvas.height)
+      }
+    })
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
@@ -34,6 +70,24 @@ function takePhoto() {
   link.setAttribute('download', '');
   strip.prepend(link);
   link.append(img);
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 100] = pixels.data[i + 0]; // RED
+    pixels.data[i + 100] = pixels.data[i + 1]; // GREEN
+    pixels.data[i - 200] = pixels.data[i + 2]; // Blue
+  }
+  return pixels;
+}
+
+function redEffects(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; // RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 100; // GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] - 100; // Blue
+  }
+  return pixels;
 }
 
 video.addEventListener('playing', drawCanvas);
